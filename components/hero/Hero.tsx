7c7628e8e8c4e9ab4e053a06e8b4541853084402
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-04 19:01:21 +0100

    Changed letter spacing in hero titles

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
             <p className={styles.hero__description}>
                 I build things for the web. Specialized in{" "}
                 <span className={styles.hero__accent}>React.js</span> and{" "}
-                <span className={styles.hero__accent}>Next.js. </span>Currently focused on looking
+                <span className={styles.hero__accent}>Next.js</span>. Currently focused on looking
                 for professional experience opportunities.
             </p>
 
