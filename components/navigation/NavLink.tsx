f2772c07d40568912cf11b7be7f179ef1a5af12c
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-04 16:35:12 +0100

    Navlink component optimization

--- components/navigation/NavLink.tsx
+++ components/navigation/NavLink.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
 import styles from "./NavLink.module.scss";
 
 type NavLinkProps = {
     title: string;
     subtitle: string;
+    href: string;
 };
 
-const NavLink = ({ title, subtitle }: NavLinkProps) => {
+const NavLink = ({ title, subtitle, href }: NavLinkProps) => {
     return (
-        <a className={styles["nav-link"]}>
+        <Link href={href} className={styles["nav-link"]}>
             <span className={styles["nav-link__subtitle"]}>{subtitle} </span> {" " + title}
-        </a>
+        </Link>
     );
 };
 
