5bf4efa777cdfb08c4eb0247710b1f566bb73c66
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-04 16:33:32 +0100

    Navigation skip button added

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,15 +1,24 @@
 import NavLink from "./NavLink";
 import styles from "./Navigation.module.scss";
+import Button from "../UI/Button";
 
 const Navigation = () => {
+    //TODO: Add resume as onClick download
+
     return (
-        <header className={styles.header}>
-            <nav className={styles.navigation}>
-                <NavLink title="About" subtitle="01." />
-                <NavLink title="Projects" subtitle="02." />
-                <NavLink title="Contact" subtitle="03." />
-            </nav>
-        </header>
+        <>
+            <a className={styles.navigation__skip} href="#hero-title">
+                Skip to Content
+            </a>
+            <header className={styles.header}>
+                <nav className={styles.navigation}>
+                    <NavLink href={"#about"} title="About" subtitle="00." />
+                    <NavLink href={"#projects"} title="Projects" subtitle="01." />
+                    <NavLink href={"#contact"} title="Contact" subtitle="02." />
+                    <Button title={"Resume"} href={"#resume"} />
+                </nav>
+            </header>
+        </>
     );
 };
 
