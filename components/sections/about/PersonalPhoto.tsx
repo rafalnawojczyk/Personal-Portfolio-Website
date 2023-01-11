dffbbcfc1a4b70a3a4b863970b7c1dfed8f796ea
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-11 17:11:12 +0100

    Added PhotoBox so all images have same styles

--- components/sections/about/PersonalPhoto.tsx
+++ components/sections/about/PersonalPhoto.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import styles from "./PersonalPhoto.module.scss";
 import Photo from "../../../public/personal-photo.jpg";
+import PhotoBox from "../../PhotoBox";
 
 const PersonalPhoto = () => {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.photo__box}>
+            <PhotoBox>
                 <Image
+                    className={styles.photo}
                     height={400}
                     width={400}
                     alt={"Photo contains a portrait of website creator - Rafał Nawojczyk"}
                     src={Photo}
                 />
-                <div className={styles.photo__overlay}></div>
-            </div>
+            </PhotoBox>
         </div>
     );
 };
