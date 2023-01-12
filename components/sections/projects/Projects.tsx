00a329c41005d7b27b5c25b93973876b8ed8d01f
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-12 13:26:58 +0100

    New projects added

--- components/sections/projects/Projects.tsx
+++ components/sections/projects/Projects.tsx
@@ -1,12 +1,14 @@
 import HomepageTitle from "../../UI/HomepageTitle";
 import FeaturedProject from "./FeaturedProject";
 import styles from "./Projects.module.scss";
+import { twitchStatisticsData, minesweeperData } from "../../../config";
 
 const Projects = () => {
     return (
         <section id="about" className={styles.about} aria-labelledby="projects">
             <HomepageTitle id="projects" title="Some Things I&#8217;ve Built" subtitle="01." />
-            <FeaturedProject />
+            <FeaturedProject data={twitchStatisticsData} />
+            <FeaturedProject data={minesweeperData} />
         </section>
     );
 };
