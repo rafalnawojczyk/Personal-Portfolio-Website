c4477d54da211938994cafbe89904a85879fdc57
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-12 13:27:53 +0100

    Code refactored to  universal, supports both sides

--- components/sections/projects/FeaturedProject.tsx
+++ components/sections/projects/FeaturedProject.tsx
@@ -1,66 +1,79 @@
 import Image from "next/image";
-import FeaturedImage from "../../../public/featured-project.jpg";
 import PhotoBox from "../../PhotoBox";
 import GitHubLogo from "../../svg/GithubLogo";
 import PreviewIcon from "../../svg/PreviewIcon";
 import styles from "./FeaturedProject.module.scss";
 
-const FeaturedProject = () => {
+type FeaturedProjectProps = {
+    data: {
+        githubLink: string;
+        previewLink: string;
+        altText: string;
+        imageSrc: string;
+        projectTitle: string;
+        projectDescription: string;
+        technologies: string[];
+        side: "left" | "right";
+    };
+};
+
+function classComposer(side: "left" | "right", className: string) {
+    return `${styles[`featured__${className}`]} ${styles[`featured__${className}--${side}`]}`;
+}
+
+const FeaturedProject = ({ data }: FeaturedProjectProps) => {
     return (
         <div className={styles.featured}>
-            <div className={styles.featured__photo}>
+            <div className={classComposer(data.side, "photo")}>
                 <PhotoBox>
                     <Image
                         height={462}
                         width={750}
                         className={styles.featured__image}
-                        alt={"Preview image showing homepage of Twitch Statistics project website"}
-                        src={FeaturedImage.src}
+                        alt={data.altText}
+                        src={data.imageSrc}
                         loading="lazy"
                     />
                 </PhotoBox>
             </div>
 
-            <div className={styles.featured__content}>
-                <p className={styles.featured__text}>Featured project</p>
-                <h3 className={styles.featured__heading}>
+            <div className={classComposer(data.side, "content")}>
+                <p className={classComposer(data.side, "text")}>Featured project</p>
+                <h3 className={classComposer(data.side, "heading")}>
                     <a
                         target="_blank"
                         rel="noreferrer"
                         className={styles.featured__title}
-                        href={"https://twitch-statistics.vercel.app/"}
+                        href={data.previewLink}
                     >
-                        Twitch Statistics
+                        {data.projectTitle}
                     </a>
                 </h3>
                 <div className={styles["featured__description-box"]}>
-                    <p className={styles.featured__description}>
-                        Advanced full-stack web application, written in Typescript using Next.js.
-                        App is fetching data from Twitch API, saves that into MongoDB using Next.js
-                        API routes, and renders all data in a modern looking charts and tables.
+                    <p className={classComposer(data.side, "description")}>
+                        {data.projectDescription}
                     </p>
                 </div>
-                <ul className={styles.featured__list}>
-                    <li>Next.js</li>
-                    <li>React</li>
-                    <li>Typescript</li>
-                    <li>SASS</li>
-                    <li>Jest</li>
-                    <li>Firebase</li>
+                <ul className={classComposer(data.side, "list")}>
+                    {data.technologies.map((el, i) => {
+                        return <li key={i}>{el}</li>;
+                    })}
                 </ul>
-                <div className={styles.featured__icons}>
+                <div className={classComposer(data.side, "icons")}>
                     <a
                         target="_blank"
                         rel="noreferrer"
-                        href="https://github.com/rafalnawojczyk/Twitch-Statistics-Project"
-                        className={`${styles["featured__icon--first"]} ${styles.featured__link}`}
+                        href={data.githubLink}
+                        className={`${styles[`featured__icon--first-${data.side}`]} ${
+                            styles.featured__link
+                        }`}
                     >
                         <GitHubLogo className={`${styles.featured__icon} `} />
                     </a>
                     <a
                         target="_blank"
                         rel="noreferrer"
-                        href="https://twitch-statistics.vercel.app/"
+                        href={data.previewLink}
                         className={styles.featured__link}
                     >
                         <PreviewIcon className={styles.featured__icon} />
