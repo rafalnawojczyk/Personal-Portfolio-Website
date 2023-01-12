563a6f48d47a0cd9629da44fa56ed68c7ba6ebc4
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-12 13:26:26 +0100

    Changed misspelled properties

--- components/svg/PreviewIcon.tsx
+++ components/svg/PreviewIcon.tsx
@@ -10,9 +10,9 @@ const PreviewIcon = ({ className }: PreviewIconProps) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className={className}
         >
             <title>External Link</title>
