7fef6368b071c1f938861b0be2ccbfce74900759
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-13 18:01:00 +0100

    Changed section ID to proper one

--- components/sections/projects/Projects.tsx
+++ components/sections/projects/Projects.tsx
@@ -10,7 +10,7 @@ import {
 
 const Projects = () => {
     return (
-        <section id="about" className={styles.about} aria-labelledby="projects">
+        <section id="project" aria-labelledby="projects">
             <HomepageTitle id="projects" title="Some Things I&#8217;ve Built" subtitle="01." />
             <FeaturedProject featured={true} side="right" data={twitchStatisticsData} />
             <FeaturedProject featured={false} side="left" data={minesweeperData} />
