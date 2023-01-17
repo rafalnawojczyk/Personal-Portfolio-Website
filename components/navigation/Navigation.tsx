6fb47085c96babe4eeca79d2a4c9ddcec1094b6c
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-17 16:07:58 +0100

    Updated mobile menu animations and logic

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -4,13 +4,27 @@ import Button from "../UI/Button";
 import { useRef, useState } from "react";
 import { CSSTransition } from "react-transition-group";
 
-const Navigation = () => {
+type NavigationProps = {
+    blurToggler: (blur: boolean) => void;
+};
+
+const Navigation = ({ blurToggler }: NavigationProps) => {
     //TODO: Add resume as onClick download
     const buttonRef = useRef(null);
+    const asideRef = useRef(null);
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     const mobileMenuToggle = () => {
-        setMobileMenuOpen(prevState => !prevState);
+        setMobileMenuOpen(prevState => {
+            blurToggler(!prevState);
+            return !prevState;
+        });
+    };
+
+    const hideMobileMenu = () => {
+        // if (!mobileMenuOpen) return;
+        setMobileMenuOpen(false);
+        blurToggler(false);
     };
 
     return (
@@ -18,7 +32,7 @@ const Navigation = () => {
             <a className={styles.navigation__skip} href="#hero-title">
                 Skip to Content
             </a>
-            <header className={styles.header}>
+            <header className={`${styles.header} ${mobileMenuOpen ? styles.blur : ""}`}>
                 <nav>
                     <div className={styles.navigation}>
                         <NavLink href={"#about"} title="About" subtitle="00." />
@@ -49,12 +63,37 @@ const Navigation = () => {
                             </CSSTransition>
                         </button>
 
-                        <aside className={styles["mobile-nav__sidebar"]}>
-                            <NavLink href={"#about"} title="About" subtitle="00." />
-                            <NavLink href={"#projects"} title="Projects" subtitle="01." />
-                            <NavLink href={"#contact"} title="Contact" subtitle="02." />
-                            <Button title={"Resume"} href={"#resume"} />
-                        </aside>
+                        <CSSTransition
+                            nodeRef={asideRef}
+                            in={mobileMenuOpen}
+                            timeout={500}
+                            classNames={{
+                                enterActive: styles["mobile-nav__sidebar-active"],
+                                enterDone: styles["mobile-nav__sidebar-active"],
+                            }}
+                        >
+                            <aside ref={asideRef} className={styles["mobile-nav__sidebar"]}>
+                                <NavLink
+                                    onClick={hideMobileMenu}
+                                    href={"#about"}
+                                    title="About"
+                                    subtitle="00."
+                                />
+                                <NavLink
+                                    onClick={hideMobileMenu}
+                                    href={"#projects"}
+                                    title="Projects"
+                                    subtitle="01."
+                                />
+                                <NavLink
+                                    onClick={hideMobileMenu}
+                                    href={"#contact"}
+                                    title="Contact"
+                                    subtitle="02."
+                                />
+                                <Button title={"Resume"} href={"#resume"} />
+                            </aside>
+                        </CSSTransition>
                     </div>
                 </nav>
             </header>
