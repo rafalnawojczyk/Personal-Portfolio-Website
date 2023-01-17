e5ab31336de69bf1b3b12a354ba3bd3dbb4cb672
rafalnawojczyk <rafalnawojczyk@gmail.com>
2023-01-17 17:18:22 +0100

    Sticky nav on desktop ver

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -3,6 +3,7 @@ import styles from "./Navigation.module.scss";
 import Button from "../UI/Button";
 import { useRef, useState } from "react";
 import { CSSTransition } from "react-transition-group";
+import { useScrollDirection } from "../../hooks/useScrollDirection";
 
 type NavigationProps = {
     blurToggler: (blur: boolean) => void;
@@ -13,6 +14,7 @@ const Navigation = ({ blurToggler }: NavigationProps) => {
     const buttonRef = useRef(null);
     const asideRef = useRef(null);
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const scrollDirection = useScrollDirection();
 
     const mobileMenuToggle = () => {
         setMobileMenuOpen(prevState => {
@@ -32,9 +34,13 @@ const Navigation = ({ blurToggler }: NavigationProps) => {
             <a className={styles.navigation__skip} href="#hero-title">
                 Skip to Content
             </a>
-            <header className={`${styles.header} ${mobileMenuOpen ? styles.blur : ""}`}>
+            <header
+                className={`${styles.header} ${mobileMenuOpen ? styles.blur : ""} ${
+                    scrollDirection === "down" ? styles.header__sticky : ""
+                } `}
+            >
                 <nav>
-                    <div className={styles.navigation}>
+                    <div className={`${styles.navigation}`}>
                         <NavLink href={"#about"} title="About" subtitle="00." />
                         <NavLink href={"#projects"} title="Projects" subtitle="01." />
                         <NavLink href={"#contact"} title="Contact" subtitle="02." />
