65ba3a51b2b910a70864d94d92f83c57f3d5a5da
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2024-07-08 23:29:50 +0200

    feat: init blog footer

---
+++ components/blog/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className={styles.separator} />
+        </footer>
+    );
+};
