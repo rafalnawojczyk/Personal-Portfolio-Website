1fa1268459c4ef2ba9b1630a8e8ae3e5602e1814
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2024-07-08 23:29:03 +0200

    feat: add mdx support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
