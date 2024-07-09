381d019f6d5fd40e616eff244f92b7ced68ef156
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2024-07-09 23:05:45 +0200

    feat: update blog footer component

--- components/blog/Footer/Footer.tsx
+++ components/blog/Footer/Footer.tsx
@@ -1,9 +1,44 @@
 import styles from './Footer.module.scss';
+import GithubLogo from './assets/Github';
+import TwitterLogo from './assets/Twitter';
+import LinkedinLogo from './assets/Linkedin';
 
 export const Footer = () => {
     return (
-        <footer>
+        <>
             <div className={styles.separator} />
-        </footer>
+            <footer className={styles.footer}>
+                <p className={styles.copyright}>© 2024 Rafal Nawojczyk</p>
+                <ul className={styles.wrapper}>
+                    <a
+                        className={styles.link}
+                        aria-label="Link to Rafal Nawojczyk - author github profile - opens in new tab"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/rafalnawojczyk"
+                    >
+                        <GithubLogo className={styles.icon} aria-disabled />
+                    </a>
+                    <a
+                        className={styles.link}
+                        aria-label="Link to Rafal Nawojczyk - author Twitter or new X profile - opens in new tab"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://x.com/rafalnawojczyk"
+                    >
+                        <TwitterLogo className={styles.icon} aria-disabled />
+                    </a>
+                    <a
+                        className={styles.link}
+                        aria-label="Link to Rafal Nawojczyk - author Linkedin profile - opens in new tab"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.linkedin.com/in/rafalnawojczyk/"
+                    >
+                        <LinkedinLogo className={styles.icon} aria-disabled />
+                    </a>
+                </ul>
+            </footer>
+        </>
     );
 };
