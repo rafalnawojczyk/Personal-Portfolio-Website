4f1f0ba07320926d13f27345214272933667e6a2
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2024-07-15 18:59:40 +0200

    feat: add metadata icons file to manage them easily

---
+++ src/utils/metadataIcons.ts
@@ -0,0 +1,38 @@
+export const metadataIcons = [
+    {
+        rel: 'icon',
+        url: '/favicon/favicon.ico',
+        sizes: '16x16',
+        type: 'image/x-icon',
+    },
+    {
+        rel: 'icon',
+        url: '/favicon/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+    },
+    {
+        rel: 'icon',
+        url: '/favicon/favicon-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+    },
+    {
+        rel: 'icon',
+        url: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+    },
+    {
+        rel: 'icon',
+        url: '/favicon/android-chrome-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+    },
+    {
+        rel: 'apple-touch-icon',
+        url: '/favicon/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+    },
+];
