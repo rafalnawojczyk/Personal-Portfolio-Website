575818fa2d7d814267e919c0d6e3d9783163e615
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2024-07-15 18:58:13 +0200

    feat: add formatDate util for blog pages

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,40 @@
+export function formatDate(date: Date): string {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const monthsOfYear = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const month = monthsOfYear[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    function getOrdinalSuffix(day: number): string {
+        if (day > 3 && day < 21) return 'th'; // Covers 11th to 20th
+        switch (day % 10) {
+            case 1:
+                return 'st';
+            case 2:
+                return 'nd';
+            case 3:
+                return 'rd';
+            default:
+                return 'th';
+        }
+    }
+
+    const ordinalSuffix = getOrdinalSuffix(day);
+
+    return `${dayOfWeek}, ${month} ${day}${ordinalSuffix}, ${year}`;
+}
