fe024d0a83aa02f67699de786c3666f84fbceb06
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2024-07-15 19:37:32 +0200

    feat: replace a with links

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,29 +4,28 @@ import styles from './Navigation.module.scss';
 import { useScrollDirection } from '@/hooks/useScrollDirection';
 import Button from '../UI/Button';
 import { Logo } from './Logo';
+import Link from 'next/link';
 
-type NavigationProps = {
-    blurToggler?: (blur: boolean) => void;
-};
+type NavigationProps = {};
 
-const Navigation = ({ blurToggler }: NavigationProps) => {
+const Navigation = ({}: NavigationProps) => {
     const scrollDirection = useScrollDirection();
 
     return (
         <>
             <header className={`${styles.header} ${scrollDirection === 'down' ? styles.header__sticky : ''} `}>
-                <a className={styles.navigation__skip} href="#hero-title">
+                <Link className={styles.navigation__skip} href="#hero-title">
                     Skip to Content
-                </a>
+                </Link>
                 <nav>
                     <div className={`${styles.navigation}`}>
                         <ul className={styles.list}>
                             <NavLink href={'/'} title="Home" subtitle="00." />
                             <NavLink href={'/blog'} title="Blog" subtitle="01." />
                         </ul>
-                        <a href="/" aria-label="Link to homepage - rafalnawojczyk.pl">
+                        <Link href="/" aria-label="Link to homepage - rafalnawojczyk.pl">
                             <Logo className={styles.logo} color={'#DADADA'} />
-                        </a>
+                        </Link>
                         <Button href="/#contact" title="Send a message" className={styles.button} />
                     </div>
                 </nav>
